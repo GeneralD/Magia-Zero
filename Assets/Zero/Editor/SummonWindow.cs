f2997134de661e9f4908cbe4f162a403e7bc0130
GeneralD <yumejustice@gmail.com>
2023-01-16 19:47:06 +0900

    feat: binding value in SummonWindow

--- Assets/Zero/Editor/SummonWindow.cs
+++ Assets/Zero/Editor/SummonWindow.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Zero {
     public class SummonWindow : EditorWindow {
+        [SerializeField] private GameObject rootObject;
+
         public static void Open() {
             var window = GetWindow<SummonWindow>(title: "Summon");
             window.ShowTab();
         }
 
         private void OnEnable() {
-            // load UXML and USS
             var visualTree = Resources.Load<VisualTreeAsset>(GetType().Name);
-            // apply them
             var root = rootVisualElement;
             visualTree.CloneTree(root);
+            root.Bind(new SerializedObject(this));
+
+            root.Q<Button>("executeButton").clicked += OnExecuteButtonClicked;
         }
 
-        private void OnGUI() { }
+        private void OnDisable() {
+            var root = rootVisualElement;
+            root.Q<Button>("executeButton").clicked -= OnExecuteButtonClicked;
+        }
+
+        private void OnExecuteButtonClicked() {
+            Debug.Log(rootObject.name);
+        }
     }
 }
