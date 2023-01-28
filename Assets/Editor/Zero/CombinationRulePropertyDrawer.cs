2a6c03f51775cf3749b3b5021137932d5718f996
GeneralD <yumejustice@gmail.com>
2023-01-28 17:34:59 +0900

    feat: approve UI

---
+++ Assets/Editor/Zero/CombinationRulePropertyDrawer.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Zero.Generator.Entity;
+
+namespace Editor.Zero {
+	[CustomPropertyDrawer(typeof(CombinationRule))]
+	public class CombinationRulePropertyDrawer : PropertyDrawer {
+		public override VisualElement CreatePropertyGUI(SerializedProperty property) {
+			var ui = new VisualElement();
+			var xml = Resources.Load<VisualTreeAsset>(GetType().Name);
+			xml.CloneTree(ui);
+			ui.Bind(property.serializedObject);
+			return ui;
+		}
+	}
+}
